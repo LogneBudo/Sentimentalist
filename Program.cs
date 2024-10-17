19e9f5d941477b12e25933f52445c01230218a47
Ivan Stankovic <istankovic@hotmail.com>
2024-10-17 04:22:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VaderSharp2;
+
+namespace Sentimentalist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentimentAnalyzer = new SentimentIntensityAnalyzer();
+            string text = "It is terrible, I love it!"; // Ambiguous text            
+            var results = sentimentAnalyzer.PolarityScores(text);
+            Console.WriteLine("Positive sentiment score: " + results.Positive);
+            Console.WriteLine("Negative sentiment score: " + results.Negative);
+            Console.WriteLine("Neutral sentiment score: " + results.Neutral);
+        }
+    }
+}
